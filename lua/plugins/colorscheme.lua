6036e483ba83eba160c6d0c3a42ba0e004a11d61
SeniorIntern <tnikhil.official@gmail.com>
2024-10-26 15:43:48 +0400

    Setup transparent theme

--- lua/plugins/colorscheme.lua
+++ lua/plugins/colorscheme.lua
@@ -2,23 +2,9 @@ return {
   "craftzdog/solarized-osaka.nvim",
   lazy = false,
   priority = 1000,
-  opts = {},
-  config = function()
-    vim.cmd.colorscheme("solarized-osaka")
+  opts = function()
+    return {
+      transparent = true,
+    }
   end,
 }
-
---[[
-return {
-  "rose-pine/neovim",
-  name = "rose-pine",
-  config = function()
-    require("rose-pine").setup({
-      disable_background = true,
-      vim.cmd.colorscheme("rose-pine"),
-      vim.api.nvim_set_hl(0, "Normal", { bg = "none" }),
-      vim.api.nvim_set_hl(0, "NormalFloat", { bg = "none" }),
-    })
-  end,
-}
-]]
